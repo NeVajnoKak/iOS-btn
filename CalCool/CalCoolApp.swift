c9f521d920195c0b77bb32c877127eea7789153d
NeVajnoKak <156511620+NeVajnoKak@users.noreply.github.com>
2024-06-29 11:44:23 +0500

    Initial Commit

---
+++ CalCool/CalCoolApp.swift
@@ -0,0 +1,17 @@
+//
+//  CalCoolApp.swift
+//  CalCool
+//
+//  Created by Erkebulan Massainov on 29.06.2024.
+//
+
+import SwiftUI
+
+@main
+struct CalCoolApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
